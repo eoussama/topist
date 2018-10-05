5706ada414dcb2a1255df01869dc24da28ff50cd
Eoussama <oussama_gtacj@live.fr>
2018-10-05 16:45:07 +0100

    Login and Register modals added

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,11 +1,32 @@
 window.addEventListener('load', () => {
     const
         btnBurger = document.getElementById('btn-burger'),
-        menuBurgered = document.getElementById('navbarMenuHeroB');
+        menuBurgered = document.getElementById('navbarMenuHeroB'),
+        btnOpenModalLogin = document.getElementById('btn-modal-login'),
+        btnCloseModalLogin = document.querySelector('#modal-login button.delete'),
+        modalLogin = document.getElementById('modal-login'),
+        btnOpenModalRegister = document.getElementById('btn-modal-register'),
+        btnCloseModalRegister = document.querySelector('#modal-register button.delete'),
+        modalRegister = document.getElementById('modal-register');
 
     btnBurger.addEventListener('click', () => {
         btnBurger.classList.toggle('is-active');
         menuBurgered.classList.toggle('is-active');
-        console.log(menuBurgered);
+    });
+
+    btnOpenModalLogin.addEventListener('click', () => {
+        modalLogin.classList.toggle('is-active');
+    });
+
+    btnCloseModalLogin.addEventListener('click', () => {
+        modalLogin.classList.toggle('is-active');
+    });
+
+    btnOpenModalRegister.addEventListener('click', () => {
+        modalRegister.classList.toggle('is-active');
+    });
+
+    btnCloseModalRegister.addEventListener('click', () => {
+        modalRegister.classList.toggle('is-active');
     });
 });
