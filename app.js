9696dd471dfafd1c022be1131d03b958b4dd7fc5
Eoussama <oussama_gtacj@live.fr>
2018-10-06 13:53:46 +0100

    exp data added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const
     express = require('express'),
-    app = express();
+    app = express(),
+    faker = require('faker'),
     PORT = process.env.PORT || 3000;
 
 // Configuration ----------------------------------------------
@@ -8,13 +9,37 @@ const
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
+var data = [];
+
+for(let i = 0; i<20; i++) {
+    let
+        _rand = Math.floor(Math.random() * 50) + 3,
+        obj = {
+            title: `TOP ${_rand} ${faker.lorem.word()}`,
+            description: faker.lorem.paragraph(),
+            date: faker.date.past(),
+            user: faker.internet.userName(),
+            upvotes: faker.random.number(),
+            downvotes: faker.random.number(),
+            views: faker.random.number(),
+            entries: []
+        };
+
+    for(let j = 0; j<_rand; j++) {
+        obj.entries.push({
+            title: faker.lorem.words(),
+            picture: faker.internet.avatar(),
+            description: faker.lorem.paragraph()
+        });
+    }
+
+    data.push(obj);
+}
 
 // Routes -----------------------------------------------------
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {data: data});
 });
 
-app.listen(PORT, () => {
-    console.log('Topist started successfully!');
-});
+app.listen(PORT, () => console.log('Topist started successfully!'));
